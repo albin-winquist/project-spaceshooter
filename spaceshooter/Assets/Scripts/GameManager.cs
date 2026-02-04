8c73e8bce277f1172120d0c554762e2bb4c3f765
exsp1ra <albin.winquist@elev.ga.lbs.se>
2026-02-04 14:53:11 +0100

    Polish

--- spaceshooter/Assets/Scripts/GameManager.cs
+++ spaceshooter/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,12 +11,30 @@ public class GameManager : MonoBehaviour
     public event Action<int> OnScoreChanged;
     public event Action<int> OnScoreGained;
 
+    [SerializeField] private int scorePerSecond = 1;
+
+    private Coroutine scoreCoroutine;
+
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        scoreCoroutine = StartCoroutine(ScoreOverTime());
+    }
+
+    private IEnumerator ScoreOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            AddScore(scorePerSecond);
+        }
     }
 
     public void AddScore(int amount)
